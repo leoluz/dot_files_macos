b6139ee39585707ed43bd2608f0d9682a2e8f20a
leoluz <leoluz@users.noreply.github.com>
2026-09-02 17:04:54 -0400

    remove home-manager git config warning
    Signed-off-by: leoluz <leoluz@users.noreply.github.com>

--- akuity/home-manager/home.nix
+++ akuity/home-manager/home.nix
@@ -63,6 +63,7 @@
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     settings."*" = {
       identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
     };
