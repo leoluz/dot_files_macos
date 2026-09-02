ce63a344791a70b210f07eae67bb3b33a04222a4
leoluz <leoluz@users.noreply.github.com>
2026-09-02 16:50:44 -0400

    port all gitconfigs to home-manager
    Signed-off-by: leoluz <leoluz@users.noreply.github.com>

--- akuity/home-manager/home.nix
+++ akuity/home-manager/home.nix
@@ -75,6 +75,31 @@
       email = "leoluz@users.noreply.github.com";
     };
 
+    settings.alias = {
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --";
+      branches = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
+    };
+
+    settings.rerere.enable = true;
+    settings.difftool.prompt = false;
+
+    settings.merge = {
+      keepBackup = false;
+      tool = "nvim";
+    };
+
+    settings.mergetool = {
+      prompt = false;
+      keepBackup = false;
+      nvim.cmd = ''nvim -f -c "DiffviewOpen"'';
+    };
+
+    settings.core.editor = "nvim";
+    settings.rebase.autosquash = true;
+    settings.url."git@github.com:".insteadOf = "https://github.com/";
+    settings.pull.ff = "only";
+    settings.push.default = "current";
+
     includes = [
       {
         condition = "gitdir:~/git/akuity/";
